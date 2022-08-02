3fe2e9575b5a01f94e6bc28be3804509e788fdd1
Michael <michaelgordoncase@gmail.com>
2022-08-02 14:02:34 -0700

    commit

--- surveys/views.py
+++ surveys/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'suverys/post_list.html', {})
+    return render(request, 'surveys/post_list.html', {})
